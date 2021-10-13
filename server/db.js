3d5f1f7a5caf580d1cbade723a4767e35c38ce65
Alex <drumlife182@gmail.com>
2021-10-13 20:14:55 +0300

    eddit db file

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const dbString =
   process.env.DATABASE_URL ||
   'postgres://qhtalimbpstjak:88248d115f3c9ec34efa52c17287f73fedecd2185bf9002da1a18337769769c1@ec2-3-230-61-252.compute-1.amazonaws.com:5432/dd0ml79lve1vs6';
 const ssl = !!process.env.DATABASE_URL;
-const db = new Sequelize(dbString, { dialectOptions: { ssl } });
+const db = new Sequelize(dbString, { dialectOptions: { ssl: true } });
 
 const Dota = db.define('dota', {
   title: Sequelize.STRING,
