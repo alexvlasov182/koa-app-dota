cbd66af18c5a0af3281723e16910f2917f3ece92
Alex <drumlife182@gmail.com>
2021-10-14 09:41:07 +0300

    fixed db

--- server/db.js
+++ server/db.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize');
 const data = require('./db.json');
 
-const dbString =
-  process.env.DATABASE_URL ||
-  'postgres://qhtalimbpstjak:88248d115f3c9ec34efa52c17287f73fedecd2185bf9002da1a18337769769c1@ec2-3-230-61-252.compute-1.amazonaws.com:5432/dd0ml79lve1vs6';
+const dbString = process.env.DATABASE_URL || '';
 const ssl = !!process.env.DATABASE_URL;
 const db = new Sequelize(dbString, { dialectOptions: { ssl } });
 
