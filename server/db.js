31bb664e2979d686c926d9c9de67662c95d94ae9
Alex <drumlife182@gmail.com>
2021-10-14 09:50:29 +0300

    eddit db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize');
 const data = require('./db.json');
 
-const dbString = process.env.DATABASE_URL || '';
+const dbString =
+  process.env.DATABASE_URL ||
+  'postgres://gdcaizikqpoyku:f108897522dcbd743faad936ee6974feeaf6e42f1d95ff6abd741367227ce02c@ec2-44-196-146-152.compute-1.amazonaws.com:5432/dbiev1k5m02g16';
 const ssl = !!process.env.DATABASE_URL;
 const db = new Sequelize(dbString, { dialectOptions: { ssl } });
 
